db7945670a185903b208001850c83585b5532d37
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-09-15 14:01:23 +0100

    Added a Todo list program

---
+++ TodoList/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello");
+Console.WriteLine("What would you like to do?");
+Console.WriteLine("[S]ee all TO DO's");
+Console.WriteLine("[A]dd a TO DO");
+Console.WriteLine("[R]emove a TO DO");
+Console.WriteLine("[E]xit TO DO's");
+
