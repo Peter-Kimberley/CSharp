9d0cfb8bca8db33783e1a441f6e9ed8955cdf3c5
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-10-01 21:04:22 +0100

    I have added a tests folder to try out differnt things in C#

---
+++ tests/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for (int i = 4; i < 99; i *= 1 )
+{
+    Console.WriteLine("This is a foorloop");
+}
+Console.ReadLine();
+// This is a forloop in C#
