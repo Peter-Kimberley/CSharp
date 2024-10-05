30b135e4e3863f9b69d8273726a1678d26b9e814
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-10-05 21:34:42 +0100

    This is practice of nested ForLoops

---
+++ tests/NestedForLoops.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+for(int i = 0; i < 4; ++i)
+{
+    for (int j = 0; j < 3; ++j)
+        Console.WriteLine($"i is {i}, j is {j}");
+}
+
+Console.WriteLine("The loop is finished");
+Console.ReadKey();
+//This is an example of a nested loop with interlopated varibles
