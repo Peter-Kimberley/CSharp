a1e0c7904bc98cd2a3837b24de133f8c1cc598e1
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-10-05 21:23:51 +0100

    This is showcasing the use of the continue statement

---
+++ tests/ContinueStatement.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+for(var i = 0; i < 20; ++i)
+{
+    if(i % 3 == 0)
+    {
+        continue;   
+    }
+    Console.WriteLine("i is " + i);
+
+}
+
+
+
+Console.WriteLine("This continue loop is now finished ");
+Console.ReadKey();
+// This is the continue condition being used to restart the loop if the current number 
+// is devisable by three.
