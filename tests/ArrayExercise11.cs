c4ff6704abd1ba4655bae59a68e65ab53b5fb9e1
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-10-13 22:13:18 +0100

    This is the latest exercise practicing with arrays

---
+++ tests/ArrayExercise11.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net;
+using System.Security.Cryptography;
+
+char[] letters = new char[] { 'h', 'e', 'l', 'l', 'o' };
+var result = "";
+for(int i = 0; i < letters.Length; ++i)
+{
+    
+    result += letters[i];
+    
+
+}
+Console.WriteLine(result);
+Console.ReadKey();
+
+    
