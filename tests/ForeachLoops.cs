d5c8970220922a048b08e02d764d5ad6b6fb8cf6
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-10-15 22:23:52 +0100

    This is the first foreach loop I have written in C#

---
+++ tests/ForeachLoops.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Tracing;
+
+Console.WriteLine("Hello, World!");
+var words = new[] { "one", "two", "three", "four", "five" };
+//for (var i = 0; i < words.Length; i++) 
+//{
+//    Console.WriteLine(words[i]);
+
+//}
+// the foreach loop is much more simplar to use, than the forloop from earlier.
+foreach(var word in words)
+{
+    Console.WriteLine(word);
+}
+
+Console.ReadKey();
