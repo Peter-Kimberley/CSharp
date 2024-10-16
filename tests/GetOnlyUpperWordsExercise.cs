97b47b789a40f67e8d8ddb408fc1f661a89775be
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-10-16 21:25:39 +0100

    A method exercise for getting upper case words from a list

---
+++ tests/GetOnlyUpperWordsExercise.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Exercise
+{
+    public List<string> GetOnlyUpperCaseWords(List<string> words)
+    {
+        var result = new List<string>();
+        foreach (var word in words)
+        {
+            if (result.Contains(word))
+            {
+                continue;
+            }
+            bool isWordUpperCase = true;
+            foreach (char letter in word)
+            {
+                if (!char.IsUpper(letter))
+                {
+                    isWordUpperCase = false;
+                }
+            }
+            if (isWordUpperCase)
+            {
+                result.Add(word);
+            }
+        }
+        return result;
+    }
+}
