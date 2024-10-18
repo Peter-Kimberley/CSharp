86a8f921cecaafa0c0f2944b514c1a1e358cce87
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-10-18 20:17:11 +0100

    This is my first look at the TryParse keyword

---
+++ tests/TryParse.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// we are parsing a string to an int
+
+bool isParsingSuccessful;
+do
+{
+
+
+    Console.WriteLine("Please enter a number");
+    var userInput = Console.ReadLine();
+
+         isParsingSuccessful = int.TryParse(
+        userInput, out int number);
+    if (isParsingSuccessful)
+    {
+        Console.WriteLine(
+            "Parsing was suceesful number is " + number);
+
+    }
+    else
+    {
+        Console.WriteLine("Parsing was not sucessful ");
+    }
+
+} while (!isParsingSuccessful);
+
+
+
+
+Console.ReadKey();
