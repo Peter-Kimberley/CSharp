08c81146bd0365aa0a80dc907d1012d42da33197
Peter Kimberley <p3t3rk1m80@gmail.com>
2024-10-20 22:25:13 +0100

    This is the first commit for my TODO project in C#

---
+++ TodoList/NewTODOList/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello");
+Console.WriteLine("What would you like to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+string userChoice = Console.ReadLine();
+
+
+
+
+Console.ReadKey();
+
